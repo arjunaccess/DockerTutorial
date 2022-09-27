cba4e7f6b724e55988587bd8aae3842176e548b9
Arjun <Arjun.Marimuthu@theaccessgroup.com>
2022-09-27 18:46:28 +0800

    Added Description in api data model

--- WebAPI/Controllers/WeatherForecastController.cs
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -11,6 +11,11 @@ namespace WebAPI.Controllers
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
+        private static readonly string[] Descriptions = new[]
+        {
+            "Desc1", "Desc2", "Desc3", "Desc4", "Desc5", "Desc6", "Desc7", "Desc8", "Desc9", "Desc10"
+        };
+
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -25,7 +30,8 @@ namespace WebAPI.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+                Description = Descriptions[Random.Shared.Next(Descriptions.Length)]
             })
             .ToArray();
         }
